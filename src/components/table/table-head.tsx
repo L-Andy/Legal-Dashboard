4c2bf7e8470ddf26914f29ec586eb3da5bdda236
L Andy <lutaayaandrew200@gmail.com>
2025-03-26 13:53:32 +0300

    Fixing table header component

--- src/components/table/table-head.tsx
+++ src/components/table/table-head.tsx
@@ -6,8 +6,8 @@ export function TableHead({ columns }: { columns: string[] }) {
     const { user } = useUser();
     return (
         <tr>
-            {columns.map((column) => (
-                <th scope="col" className="px-6 py-3 text-left text-xs font-semibold text-primary uppercase tracking-wider">{column}</th>
+            {columns.map((column, index) => (
+                <th scope="col" key={index} className="px-6 py-3 text-left text-xs font-semibold text-primary uppercase tracking-wider">{column}</th>
             ))}
             <th scope="col" className={`px-6 py-3 text-right text-xs font-semibold text-primary uppercase tracking-wider ${user?.role !== RolesEnum.ADMIN ? 'hidden' : ''}`}>Actions</th>
         </tr>
